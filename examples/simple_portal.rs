32c6c8c4922b3e1ca8a551f1b9f70860ee8e3008
addonovan <austin@addonovan.com>
2018-04-18 01:20:23 -0500

    Ported majority of simple portal example

--- examples/simple_portal.rs
+++ examples/simple_portal.rs
@@ -1,18 +1,13 @@
 extern crate mwf;
 
-use mwf::{View, ServerBuilder};
+use mwf::{View, ViewResult, RequestHandler, ServerBuilder};
 use std::sync::{Arc, Mutex};
 use std::error::Error;
 use std::fmt;
+use mwf::routing::RouteMap;
 
 #[derive(Debug)]
 struct NoSuchUserError;
-
-/// A user portal, i.e. a list of users
-struct UserPortal
-{
-    users: Vec<User>,
-}
 
 /// A user in our "system"
 struct User
@@ -26,6 +21,52 @@ struct User
     name: String,
 }
 
+struct UserPortal
+{
+    users: Arc<Mutex<Vec<User>>>,
+}
+
+impl UserPortal
+{
+    fn new(users: Vec<User>) -> Self
+    {
+        UserPortal {
+            users: Arc::new(Mutex::new(users)),
+        }
+    }
+}
+
+impl RequestHandler for UserPortal
+{
+    fn handle(&self, route_map: RouteMap) -> ViewResult
+    {
+        // grab the deets from the URL
+        let id: u32 = route_map[":id"].parse().unwrap();
+        let action = route_map[":action"].as_str();
+
+        let users = self.users.lock().unwrap();
+
+        // grab the user specified by the id
+        let user = match users.iter().find(|it| it.id == id) {
+            None => return Err(Box::new(NoSuchUserError::new())),
+            Some(x) => x,
+        };
+
+        // try to perform the given action
+        let text: String = match action {
+            "greet" => {
+                format!("Hello, {}!", user.name)
+            },
+
+            _ => {
+                "Unknown action :(".into()
+            }
+        };
+
+        View::from(text)
+    }
+}
+
 fn main()
 {
     // generate a list of user information, realistically, this would
@@ -37,120 +78,14 @@ fn main()
 
     // create a user portal using the users we made
     // we'll wrap it in an arc-mutex so that it's thread-safe
-    let portal = Arc::new(Mutex::new(UserPortal::new(users)));
-
-    // we have to make clones of portal before we start, so that the clones
-    // can each be moved safely into the closure and owned by them
-    let context1 = portal.clone();
-    let context2 = portal.clone();
-    let context3 = portal.clone();
-    let context4 = portal.clone();
+    let portal = UserPortal::new(users);
 
     ServerBuilder::new()
-        // if we're just supplied the user's id, we'll just call the view_user
-        // method on the UserPortal (everything else is just boilerplate and
-        // error handling)
-        .on_page("/user/:id/", move |args| {
-            let id: u32 = args[":id"].parse()?;
-
-            // if the text couldn't be generated, then the user
-            // with that id didn't exist
-            let text = match context1.lock().unwrap().view_user(id) {
-                None => return Err(Box::new(NoSuchUserError::new())),
-                Some(x) => x,
-            };
-
-            View::from(text)
-        })
-        // if we're asked to greet the user, we'll say hi to them
-        .on_page("/user/:id/greet", move |args| {
-            let id: u32 = args[":id"].parse()?;
-            let portal = context2.lock().unwrap();
-
-            // make sure that the user exists
-            let user = match portal.user(id) {
-                None => return Err(Box::new(NoSuchUserError::new())),
-                Some(x) => x,
-            };
-
-            let text = format!("Hello, {} (id = {})", user.name, user.id);
-
-            View::from(text)
-        })
-        // if we're supposed to edit the id of the user, we also need the
-        // new id. Then, we'll change the user's id
-        .on_page("/user/:id/edit/id/:new_id", move |args| {
-            let id: u32 = args[":id"].parse()?;
-            let new_id: u32 = args[":new_id"].parse()?;
-
-            let mut portal = context3.lock().unwrap();
-
-            // make sure that the user exists
-            let user = match portal.user_mut(id) {
-                None => return Err(Box::new(NoSuchUserError::new())),
-                Some(x) => x,
-            };
-
-            // realistically, we should check if another user has that
-            // same new_id, but oh well, this is just an example of the
-            // software, not an actual service
-            user.id = new_id;
-
-            View::from("Success!")
-        })
-        // same as above, but for the user's name
-        .on_page("/user/:id/edit/name/:new_name", move |args| {
-            let id: u32 = args[":id"].parse()?;
-            let new_name = args[":new_name"].clone();
-
-            let mut portal = context4.lock().unwrap();
-
-            // make sure that the user exists
-            let user = match portal.user_mut(id) {
-                None => return Err(Box::new(NoSuchUserError::new())),
-                Some(x) => x,
-            };
-
-            // realistically, we should check if another user has that
-            // same new_id, but oh well, this is just an example of the
-            // software, not an actual service
-            user.name = new_name;
-
-            View::from("Success!")
-        })
+        .bind("/user/:id/:action", portal)
         .start()
         .unwrap();
 }
 
-impl UserPortal
-{
-    fn new(users: Vec<User>) -> UserPortal
-    {
-        UserPortal {
-            users
-        }
-    }
-
-    fn user(&self, id: u32) -> Option<&User>
-    {
-        self.users.iter()
-            .find(|it| it.id == id)
-    }
-
-    fn user_mut(&mut self, id: u32) -> Option<&mut User>
-    {
-        self.users.iter_mut()
-            .find(|it| it.id == id)
-    }
-
-    fn view_user(&self, id: u32) -> Option<String>
-    {
-        self.user(id)
-            .and_then(|user| {
-                Some(format!("Your name is, {}", user.name))
-            })
-    }
-}
 
 impl User
 {
